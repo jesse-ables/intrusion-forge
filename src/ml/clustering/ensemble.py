1c02b283a52fa1ea2691a9cea4abbc31789a543a
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2026-05-13 15:03:01 +0200

    Merge: clustering ensemble algorithm (clean)

--- src/ml/clustering/ensemble.py
+++ src/ml/clustering/ensemble.py
@@ -1,76 +1,4 @@
-if __name__ == '__main__':
-    from base import *
-else:
-    from .base import *
-
-from time import perf_counter_ns
-import os
-from pathlib import Path
-
-# TESTING
-from collections import defaultdict
-from collections import defaultdict
-import numpy as np
-
-
-def class_index_sets(arr):
-    '''
-    prende array e returna un dizionario dove ogni cluster id è la chiave
-    e value la lista degli indici in cui appare
-    '''
-    groups = defaultdict(set)
-
-    for idx, cls in enumerate(arr):
-        groups[cls].add(idx)
-
-    return groups
-
-
-def compare_clusters(cl1_di_labels_list : dict[int, list[int]], cl2_ri2cid : dict[int, list[int]], cluster_mappings : dict[int, int]):
-    '''
-    prende cl1 e cl2 da class_index_sets e usando cluster_mappings cerca di capire
-    se i cluster sono identici
-
-    cluster_mappings ha key il cluster di cl2_ri2cid e value quello di cl1_di_labels_list
-    '''
-    res = True    
-
-    for clid2 in list(cl2_ri2cid.keys())[:]:
-        if cluster_mappings[clid2] == "skip this":
-            print("SKIPPED SENTINEL...")
-            continue 
-        clid1 = cluster_mappings[clid2] # il cluster id di algoritmo_clust_usato
-        indexes2 = cl2_ri2cid[clid2]
-        indexes1 = cl1_di_labels_list[clid1]
-
-        print(f"DIFFERENT LEN: algoritmo_clust_usato={len(indexes1)}, ensemble={len(indexes2)}") if len(indexes1) != len(indexes2) else None
-
-        only_in_indexes1 = set(indexes1) - set(indexes2)
-        only_in_indexes2 = set(indexes2) - set(indexes1)
-
-        print(f"Only in indexes algoritmo_clust_usato (ens={clid2},hdb={clid1}) -> {len(only_in_indexes1)} items.") if len(only_in_indexes1) > 0 else None
-        print(f"Only in indexes ensemble (ens={clid2},hdb={clid1}) -> {len(only_in_indexes2)} items.") if len(only_in_indexes2) > 0 else None
-
-        if len(only_in_indexes1)!=0 or len(only_in_indexes2)!=0:
-            res = False
-
-        del cl2_ri2cid[clid2]
-        del cl1_di_labels_list[clid1]
-
-    if len(cl1_di_labels_list.keys())!=0:
-        print(f"Rimasta roba in cl1: {cl1_di_labels_list.keys()}")
-        res = False
-
-    if len(cl2_ri2cid.keys())!=0:
-        print(f"Rimasta roba in cl2: {cl2_ri2cid.keys()}")
-        res = False
-
-    return res
-
-
-
-# END OF TESTING
-
+from .base import *
 
 def are_the_labels_all_the_same_len(labels_list: list[np.ndarray]):
     length = labels_list[0].shape[0]
@@ -78,33 +6,6 @@ def are_the_labels_all_the_same_len(labels_list: list[np.ndarray]):
         if length != label.shape[0]:
             return False
     return True
-
-
-def split_into_ranges(range_start, range_end, amount_of_ranges_to_split_into) -> list[tuple[int, int]]:
-    """
-    Splits a numeric interval [range_start, range_end) into approximately equal
-    contiguous sub-ranges.
-
-    The function returns a list of tuples representing sub-ranges such as:
-    [(start1, end1), (start2, end2), ...] covering the full interval without gaps.
-
-    If the interval cannot be divided evenly, the first ranges receive one extra unit.
-    """
-
-    total_length = range_end - range_start
-    base = total_length // amount_of_ranges_to_split_into
-    remainder = total_length % amount_of_ranges_to_split_into
-
-    ranges = []
-    start = range_start
-
-    for i in range(amount_of_ranges_to_split_into):
-        end = start + base + (1 if i < remainder else 0)
-        ranges.append((start, end))
-        start = end
-
-    return ranges
-
 
 def assign_cluster_id_to_array_in_place(row_index_to_cluster_id : np.ndarray, 
                                         labels_list : list[np.ndarray], 
@@ -124,8 +25,6 @@ def assign_cluster_id_to_array_in_place(row_index_to_cluster_id : np.ndarray,
     labels_amount = float(len(labels_list)) # The amount of clustering algorithms used, inferred by how many labels are received
     
     for comparative_index in range(start_index, end_index):
-        # print(f"Idx1={first_compare_index},Cl_ID1={current_cluster_id},Idx2={comparative_index}/{end_index},", end='\r', flush=True)
-
         votes_for_same_cluster = 0.0 # How many clustering algorithms vote for the two rows to be in the same cluster
         # Find the vote of each clustering algorithm
         for i in range(len(labels_list)):
@@ -133,14 +32,6 @@ def assign_cluster_id_to_array_in_place(row_index_to_cluster_id : np.ndarray,
             
             current_index_cluster_id_in_this_label = label[first_compare_index]
             comparative_index_cluster_id_in_this_label = label[comparative_index]
-            # print('\n', i, '->', current_index_cluster_id_in_this_label, '==', comparative_index_cluster_id_in_this_label)
-            
-            # TODO ABSTAINED HANDLING: if the current labeling for the comparative index is "abstained",
-            # then no vote is given at all
-            if abstained_label_per_labels is not None:
-                current_abstained_label = abstained_label_per_labels[i]
-                if current_abstained_label is not None and comparative_index_cluster_id_in_this_label == current_abstained_label:
-                    continue
 
             # Are they assigned to the same cluster?
             if current_index_cluster_id_in_this_label == comparative_index_cluster_id_in_this_label:
@@ -148,17 +39,11 @@ def assign_cluster_id_to_array_in_place(row_index_to_cluster_id : np.ndarray,
              
         # Once votes are given, get the final verdict
         if votes_for_same_cluster/labels_amount >= threshold:
-            # print(f"Had {votes_for_same_cluster} votes with {labels_amount} labels, and threshold {threshold}")
             # Same clusters, so, fill the final array
             row_index_to_cluster_id[comparative_index] = current_cluster_id
-            # print(f"Idx1={first_compare_index},Cl_ID1={current_cluster_id},Idx2={comparative_index}/{end_index},Cl_ID2=ASSIGNED", end='\r', flush=True)
-        else:
-            # print(f"Idx1={first_compare_index},Cl_ID1={current_cluster_id},Idx2={comparative_index}/{end_index},Cl_ID2=--------", end='\r', flush=True)
-            pass
 
 
 def compute_ensemble_labels(labels_list: list[np.ndarray],
-                            abstained_label_per_labels: list,
                             threshold: float = 0.5) -> np.ndarray:
     '''
 
@@ -173,9 +58,6 @@ def compute_ensemble_labels(labels_list: list[np.ndarray],
     unreasonable to happen.
 
     You should always use threads_number = 1.
-
-    TODO chiedi a simone dove vanno a finire le row senza cluster, tipo quelle che hdbscan
-    mette a -1
 
     params:
         labels_list: list of the labels assigned by each clustering algorithm. One label per clustering algorithm.
@@ -192,71 +74,37 @@ def compute_ensemble_labels(labels_list: list[np.ndarray],
         raise ValueError(f"The labels have different lengths. Cannot ensemble: {[label.shape[0] for label in labels_list]}")
 
     rows_amount = labels_list[0].shape[0] # Number of rows in the dataframe.
-    # Cannot use -1 to fill it, because it is used by some algorithms.
+    
     sentinel = np.iinfo(np.int32).min # The value that is used as a filler to mark a "unfilled" array cell
+                                      # Cannot use -1 to fill it, because it is used by some algorithms.
     row_index_to_cluster_id = np.full(rows_amount, sentinel, dtype=np.int32) # Array that maps a row index to its cluster id
 
     next_clust_id = np.int32(0) # The id to assign to the next cluster, if a new one is found
 
-    start_time = perf_counter_ns() # TODO remove.
-    cluster_mappings = {sentinel : "skip this"} # TODO remove. mappa clust id nuovi ai vecchi
-
     # Iterate over each row by index, and assign its cluster instantly
     for current_index in range(rows_amount):
-        current_index_time_start = perf_counter_ns() # TODO remove.
-        # print(f"Idx1={current_index},", end='\r', flush=True) # TODO remove.
         current_cluster_id = row_index_to_cluster_id[current_index] # Get the current cluster id, if it was assigned
         # If no cluster id is given, assign it the next cluster id, which initiates a new cluster
         if current_cluster_id == sentinel:
             current_cluster_id = next_clust_id # Assign the correct current cluster id for the iteration
-            cluster_mappings[current_cluster_id] = labels_list[0][current_index] # TODO remove.
             row_index_to_cluster_id[current_index] = current_cluster_id # Fill the final array in the current index p
             next_clust_id += 1 # Increase the cluster id for a future new cluster
         else:
             # A cluster id has been assigned already, I can skip this row
             continue
-            
-        # print(f"Idx1={current_index},Cl_ID1={current_cluster_id}", end='\r', flush=True)
 
         # Assign the cluster of the next elements of row_index_to_cluster_id, if and only if
         # they are assigned to the same cluster by MAJORITY vote
         assign_cluster_id_to_array_in_place(
             row_index_to_cluster_id=row_index_to_cluster_id, 
-            labels_list=labels_list, 
-            abstained_label_per_labels=abstained_label_per_labels,
+            labels_list=labels_list,
             first_compare_index=current_index,
             start_index=current_index+1, 
             end_index=rows_amount, 
             current_cluster_id=current_cluster_id,
             threshold=threshold
         )
-
-        current_index_time_elapsed = perf_counter_ns() - current_index_time_start
-        # print(f"IT TOOK {current_index_time_elapsed} FOR INDEX {current_index}/{rows_amount}.")
-
-    for el in row_index_to_cluster_id:
-        assert(el != sentinel)
-
-    end_time_elapsed = perf_counter_ns() - start_time
-
-    print(f"IT TOOK {end_time_elapsed} FOR THE WHOLE THINGY")
-    max_clusters = 0
-    for label in labels_list:
-        if np.unique(label).size > max_clusters:
-            max_clusters = np.unique(label).size
-
-    print("ALGO_USATO CHECK SINGLE")
-    import pandas as pd
-    df = pd.DataFrame(labels_list[0], columns=["algoritmo_clust_usato"])
-    print(df.value_counts())
-    df = pd.DataFrame(row_index_to_cluster_id, columns=["ri2cid"])
-    print(df.value_counts())
     
-    if compare_clusters(cl1_di_labels_list=class_index_sets(arr=labels_list[0]), cl2_ri2cid=class_index_sets(row_index_to_cluster_id), cluster_mappings=cluster_mappings):
-        print("YES THE SAME")
-    else:
-        print("NO DIFFERENT... DIDNT WORK MAN")
-    exit("CIAONE") 
     return row_index_to_cluster_id
 
 def make_ensemble_cluster_fn(
@@ -267,29 +115,9 @@ def make_ensemble_cluster_fn(
 
     def _fn(X: np.ndarray) -> np.ndarray:
         labels_list = [cluster_fn(X) for cluster_fn in cluster_fns]
-
-        # save
-        filepath = Path(os.path.abspath(os.path.dirname(__file__))) / 'kmeans_gmm.npz' 
-        np.savez(filepath, *labels_list)
-        exit("SAVED KMEANS")
         
         labels = compute_ensemble_labels(labels_list=labels_list, threshold=threshold)
         return labels
 
     return _fn
 
-
-
-if __name__ == '__main__':
-    # load
-    filepatha = Path(os.path.abspath(os.path.dirname(__file__))) / 'kmeans_gmm.npz' 
-    loadeda = np.load(filepatha)
-    labels_lista = [loadeda[key] for key in loadeda]
-
-    # abstain labels
-    hdbscan_abstained_lista = [np.int32(-1)] # None
-    abstained_lista = None
-
-    # ensemble
-    labelsa = compute_ensemble_labels(labels_list=labels_lista, 
-                                      abstained_label_per_labels=abstained_lista)
